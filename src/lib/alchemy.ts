341775b052e2303b0947f80d324e492d753a400c
Samk <90044455+Sam-ruk@users.noreply.github.com>
2025-06-20 19:05:17 +0530

    Delete src/lib directory

--- src/lib/alchemy.ts
+++
@@ -1,9 +0,0 @@
-import { Alchemy, Network } from "alchemy-sdk";
-
-const alchemy = new Alchemy({ 
-	apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY, 
-	network: Network.MONAD_TESTNET,
-	url: process.env.NEXT_PUBLIC_MONAD_WSS
-});
-
-export default alchemy; // only 1 export so while importing no braces
